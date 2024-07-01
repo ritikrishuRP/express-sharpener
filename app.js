33061ddeb09d9f92552cbfa8a8a42513182c002e
rrishu212 <rrishu212@gmail.com>
2024-07-01 17:17:20 +0530

    body-parser

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next)=>{
-    console.log('In the middleware!');
-    next();   //Allows the middleware conitinue to the next middleware.
-}); //allows us to add middleware
+app.use(bodyParser.urlencoded({extended: false}))
 
-app.use((req,res,next)=>{
+
+
+app.use('/add-product',(req,res,next)=>{
     console.log('In another middleware!');
-    res.send('<h1>Hello from express!</h1>');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"></input></input><button type = "submit">Add Product</button></form>')
+});
 
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/')
+})
+
+app.use('/',(req,res,next)=>{
+    res.send('<h1>Hello from express!</h1>');
 });
 
 app.listen(3000);
