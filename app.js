72a06d36567913fd57c308dee5f36bc3fa3f0833
rrishu212 <rrishu212@gmail.com>
2024-07-02 10:37:39 +0530

    routes

--- app.js
+++ app.js
@@ -3,22 +3,18 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin.js')
+const shopRoutes = require('./routes/shop.js')
+
 app.use(bodyParser.urlencoded({extended: false}))
 
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
 
-
-app.use('/add-product',(req,res,next)=>{
-    console.log('In another middleware!');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"></input></input><button type = "submit">Add Product</button></form>')
-});
-
-app.use('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/')
+app.use((req,res,next) => {
+    res.status(404).send('<h1>Page not found</h1>')
 })
 
-app.use('/',(req,res,next)=>{
-    res.send('<h1>Hello from express!</h1>');
-});
+
 
 app.listen(3000);
